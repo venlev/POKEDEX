a7298089fb84e5c4d4128c4fcb1edf52eda885da
Francis <veneghegyi.levente@gmail.com>
2023-09-14 20:13:20 +0200

    added dotenv

--- src/components/welcome-panel/welcome-panel.component.tsx
+++ src/components/welcome-panel/welcome-panel.component.tsx
@@ -4,6 +4,8 @@ import Chip from '@mui/material/Chip';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import SpaIcon from '@mui/icons-material/Spa';
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
+import SaveIcon from '@mui/icons-material/Save';
+import Fab from '@mui/material/Fab';
 
 const WelcomePanel = () => {
 
@@ -16,11 +18,15 @@ const WelcomePanel = () => {
             <div id="favourite-pokemon-wrapper">
                 <h3 className='name-inquiry'>What are your favourite pokemon types?</h3>
                 <div id="pokemon-type-selection">
-                    <Chip icon={<WhatshotIcon />} label="Fire" color='warning'/>
-                    <Chip icon={<SpaIcon />} label="Grass" color='success'/>
-                    <Chip icon={<ElectricBoltIcon />} label="Electric" color='primary'/>
+                    <Chip icon={<WhatshotIcon />} label="Fire" color='warning' />
+                    <Chip icon={<SpaIcon />} label="Grass" color='success' />
+                    <Chip icon={<ElectricBoltIcon />} label="Electric" color='primary' />
                 </div>
             </div>
+            <Fab variant="extended">
+                <SaveIcon sx={{ mr: 1 }} />
+                Save my account
+            </Fab>
         </div>
     )
 }
