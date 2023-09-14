4c7d6e0b7cf55dd529864ae5fc9ccf9ad8c8e466
Francis <veneghegyi.levente@gmail.com>
2023-09-14 20:55:15 +0200

    basic firestore config

--- src/components/welcome-panel/welcome-panel.component.tsx
+++ src/components/welcome-panel/welcome-panel.component.tsx
@@ -6,14 +6,30 @@ import SpaIcon from '@mui/icons-material/Spa';
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 import SaveIcon from '@mui/icons-material/Save';
 import Fab from '@mui/material/Fab';
+import { useEffect, useState } from 'react';
+import { db } from '../../firebase';
+import { collection, onSnapshot } from '@firebase/firestore';
 
 const WelcomePanel = () => {
+
+    const [nickname, setNickname] = useState('');
+    const [favouritePokemonTypes, setFavouritePokemonTypes] = useState([]);
+
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        const userDataCallSubscription = onSnapshot(collection(db, 'user-data'), (dbRecords) => {
+            //gets data from db collection dbRecords.docs.map(document=>document.data())
+        });
+
+        return userDataCallSubscription;
+    }, []);
 
     return (
         <div id='welcome-panel-container'>
             <div id="name-inquiry-wrapper">
                 <h3 className='name-inquiry'>What is your name?</h3>
-                <TextField label="Nickname" variant="outlined" color='secondary' placeholder='Mr. Raptor' />
+                <TextField label="Nickname" variant="outlined" color='secondary' placeholder='Mr. Raptor' value={nickname} onChange={(e) => setNickname(e.target.value)} />
             </div>
             <div id="favourite-pokemon-wrapper">
                 <h3 className='name-inquiry'>What are your favourite pokemon types?</h3>
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import 'firebase/auth';
-import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
